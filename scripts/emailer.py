cfd1195feb4319f5f5fab6bb1638d7d8913e5bd8
ATRIX GAMING <theokazmaros@gmail.com>
2023-07-19 11:57:24 +1000

    upgraded emailer, subject now showing properly

--- scripts/emailer.py
+++ scripts/emailer.py
@@ -1,4 +1,6 @@
 import smtplib, ssl
+from email.message import EmailMessage
+
 
 class Emailer:
     def send_email(domain, sender, receiver, sender_password):
@@ -7,10 +9,15 @@ class Emailer:
         sender_email = sender  
         receiver_email = receiver 
         password = sender_password
-        message = f"""
-        {domain} is now available!"""
+        msg = EmailMessage()
+        msg.set_content(f'{domain} is now available!')
+
+        msg['Subject'] = f'{domain} is now available!'
+        msg['From'] = sender_email
+        msg['To'] = receiver_email
 
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
             server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message)
+            server.send_message(msg)
+            server.quit()
